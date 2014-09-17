7601185a30ca687e9ea265fdaa9362cf98b1e777
Harry Jordan <harry@inquisitivesoftware.com>
2014-09-17 10:46:20 +0100

    Initial Commit

---
+++ SnowballStemmer/SnowballStemmer.h
@@ -0,0 +1,19 @@
+//
+//  SnowballStemmer.h
+//  SnowballStemmer
+//
+//  Created by Harry Jordan on 17/09/2014.
+//  Copyright (c) 2014 InquisitiveSoftware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SnowballStemmer.
+FOUNDATION_EXPORT double SnowballStemmerVersionNumber;
+
+//! Project version string for SnowballStemmer.
+FOUNDATION_EXPORT const unsigned char SnowballStemmerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SnowballStemmer/PublicHeader.h>
+
+
